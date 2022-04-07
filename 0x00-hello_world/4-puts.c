d9ff2ccd3fb73a2879825ed907d2790009c5b12e
Onyinyechi6052 <abigailclassic@gmail.com>
2022-04-07 13:16:50 -0700

    My commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,10 @@
 /**
  *main - entry point
  *
+ *Return: 0 after printing the function
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0)
+return (0);
 }
